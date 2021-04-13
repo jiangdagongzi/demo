4e52795205224e243eda0a1d3bc7b647ad558daf
jiangdagongzi <34465648+jiangdagongzi@users.noreply.github.com>
2021-04-13 23:56:31 +0800

    add todo

--- src/main/java/com/tao/jiang/demo/utils/general/TimeUtils.java
+++ src/main/java/com/tao/jiang/demo/utils/general/TimeUtils.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 public class TimeUtils {
 
+
+    // TODO fix logic
     public static boolean timeSlotLongerThan(Date date1, Date date2, int timeSlot) {
         return false;
     }
